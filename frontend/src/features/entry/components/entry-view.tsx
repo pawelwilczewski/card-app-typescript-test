c6c8e0b0e36ae820cb0273234de89dd808d1c37c
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2025-02-06 15:06:44 +0000

    Fixed entry view styling

--- frontend/src/features/entry/components/entry-view.tsx
+++ frontend/src/features/entry/components/entry-view.tsx
@@ -1,5 +1,6 @@
 import PrimitiveButton from "@/features/shared/components/primitive-button";
 import { useContext } from "react";
+import { BiPencil, BiTrash } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
 import { EntryContext } from "../context/entry-context";
 import { Entry } from "../types/entry";
@@ -14,30 +15,32 @@ const EntryView: React.FC<EntryViewProps> = ({ entry }) => {
 
   return (
     <div className="bg-card text-card-foreground m-3 p-4 rounded flex flex-col justify-between">
-      <h1 className="font-bold text-sm md:text-lg">{entry.title}</h1>
+      <h1 className="font-bold text-lg break-words overflow-auto">{entry.title}</h1>
       <p className="text-center text-lg font-light md:mt-2 md:mb-4 mt-1 mb-3">{entry.description}</p>
-      <div className="flex items-center justify-between flex-col md:flex-row pt-2 md:pt-0">
-        <div className="flex justify-center">
+      <div className="flex justify-between flex-wrap gap-2">
+        <time className="block self-end">{new Date(entry.created_at.toString()).toLocaleDateString()}</time>
+        <div className="flex justify-center ml-auto">
           <PrimitiveButton
             variant="ghost"
-            onClick={() => {
-              deleteEntry(entry.id as string);
-            }}
-          >
-            ✖
-          </PrimitiveButton>
-          <PrimitiveButton
-            variant="ghost"
+            size="small"
+            className="!text-card-foreground hover:bg-card-foreground hover:!text-primary-foreground"
             onClick={() => {
               navigate(`/edit/${entry.id}`, { replace: true });
             }}
           >
-            🖊
+            <BiPencil size="1.5rem" />
+          </PrimitiveButton>
+          <PrimitiveButton
+            variant="ghost"
+            size="small"
+            className="!text-card-foreground hover:bg-card-foreground hover:!text-primary-foreground"
+            onClick={() => {
+              deleteEntry(entry.id as string);
+            }}
+          >
+            <BiTrash size="1.5rem" />
           </PrimitiveButton>
         </div>
-        <time className="text-right text-sm md:text-lg">
-          {new Date(entry.created_at.toString()).toLocaleDateString()}
-        </time>
       </div>
     </div>
   );
