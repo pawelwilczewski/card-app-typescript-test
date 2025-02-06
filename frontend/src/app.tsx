3eda32fe88465c3505c3717a5677f16dc29dd3b8
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2025-02-06 02:59:12 +0000

    Fixed vite types resolution

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,10 +1,10 @@
+import { EntryProvider } from "@/features/entry/context/entry-context";
+import AllEntries from "@/features/entry/routes/all-entries";
+import EditEntry from "@/features/entry/routes/edit-entry";
+import NewEntry from "@/features/entry/routes/new-entry";
+import NavBar from "@/features/shared/components/nav-bar";
+import { ThemeProvider } from "@/features/theme/context/theme-context";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import { EntryProvider } from "./features/entry/context/entry-context";
-import AllEntries from "./features/entry/routes/all-entries";
-import EditEntry from "./features/entry/routes/edit-entry";
-import NewEntry from "./features/entry/routes/new-entry";
-import NavBar from "./features/shared/components/nav-bar";
-import { ThemeProvider } from "./features/theme/context/theme-context";
 
 export default function App() {
   return (
