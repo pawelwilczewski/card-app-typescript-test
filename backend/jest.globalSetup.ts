745f1c929427e36b5ecf81856bccbe2f364b42d5
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2025-02-06 22:39:04 +0000

    Set up testing environment to use a separate db

---
+++ backend/jest.globalSetup.ts
@@ -0,0 +1,10 @@
+// jest.globalSetup.ts
+import dotenv from "dotenv";
+import { execSync } from "node:child_process";
+import path from "node:path";
+
+dotenv.config({ path: path.resolve(process.cwd(), ".env.test") });
+
+export default async function globalSetup(): Promise<void> {
+  execSync("npx prisma migrate deploy", { stdio: "inherit" });
+}
